88070012c2d853a51b9667cba33685cd9607c271
Soham Pal <sohampal101@gmail.com>
2021-04-14 21:50:09 +0530

    UPD: Cleared unutilized files

--- groww-go/src/actions/cheese.jsx
+++
@@ -1,8 +0,0 @@
-import { Text } from '../components/chatbot/components/text';
-import React from 'react'
-
-export default class extends React.Component {
-  render() {
-    return <Text>You chose Cheese on Pasta</Text>
-  }
-}
--- groww-go/src/components/chatbot/util/message-utils.js
+++
@@ -1,9 +0,0 @@
-import { INPUT } from '../constants';
-
-
-export const isOfType = (msgType, type) => msgType === type
-
-export const isText = msg => isOfType(msg.type, INPUT.TEXT)
-export const isPostback = msg => isOfType(msg.type, INPUT.POSTBACK)
-export const isCustom = msg => isOfType(msg.type, INPUT.CUSTOM)
-export const isButtonMessage = msg => isOfType(msg.type, INPUT.BUTTON_MESSAGE)
