6224ffbb536a1783e754f5f19e86f806349f3bd4
Soham Pal <sohampal101@gmail.com>
2021-04-14 14:26:07 +0530

    UPD: Stocks final

--- backend/src/stocks/dto/create-stock.dto.ts
+++ backend/src/stocks/dto/create-stock.dto.ts
@@ -1,18 +1,72 @@
 import { IsNotEmpty } from 'class-validator';
 
-class prevPrice {
+class LivePriceDto {
+  type: string;
+  symbol: string;
+  tsInMillis: number;
+  open: number;
+  high: number;
+  low: number;
   close: number;
   ltp: number;
-  tsInMills: number;
+  dayChange: number;
+  dayChangePerc: number;
+  lowPriceRange: number;
+  highPriceRange: number;
+  volume: number;
+  totalBuyQty: number;
+  totalSellQty: number;
 }
 
 export class CreateStockDto {
   @IsNotEmpty()
-  readonly name: string;
+  isin: string;
 
   @IsNotEmpty()
-  readonly currentPrice: number;
+  companyName: string;
 
   @IsNotEmpty()
-  readonly previousPrices: prevPrice[];
+  companyLogo: string;
+
+  @IsNotEmpty()
+  companyDescription: string;
+
+  @IsNotEmpty()
+  bseScriptCode: number;
+
+  @IsNotEmpty()
+  nseScriptCode: string;
+
+  @IsNotEmpty()
+  yearlyHighPrice: number;
+
+  @IsNotEmpty()
+  yearlyLowPrice: number;
+
+  @IsNotEmpty()
+  marketCap: number;
+
+  @IsNotEmpty()
+  pb: number;
+
+  @IsNotEmpty()
+  pe: number;
+
+  @IsNotEmpty()
+  industryPe: number;
+
+  @IsNotEmpty()
+  divYield: number;
+
+  @IsNotEmpty()
+  bookValue: number;
+
+  @IsNotEmpty()
+  roe: number;
+
+  @IsNotEmpty()
+  eps: number;
+
+  @IsNotEmpty()
+  livePriceDto: LivePriceDto;
 }
