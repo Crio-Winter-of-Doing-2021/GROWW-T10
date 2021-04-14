c9dc392ba3aa32299632b936e170c0af7f30b16f
Soham Pal <sohampal101@gmail.com>
2021-04-14 15:26:09 +0530

    ADD: Us Stocks DTO

--- backend/src/us-stocks/dto/update-us-stock.dto.ts
+++ backend/src/us-stocks/dto/update-us-stock.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateUsStockDto } from './create-us-stock.dto';
 
-export class UpdateUsStockDto extends PartialType(CreateUsStockDto) {}
+export class UpdateUsStockDto extends PartialType(
+  OmitType(CreateUsStockDto, ['isin'] as const),
+) {}
