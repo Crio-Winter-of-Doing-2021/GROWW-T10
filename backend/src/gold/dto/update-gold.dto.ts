bdfa127566f1a4883e63b2d4458b3b2b394521e9
Soham Pal <sohampal101@gmail.com>
2021-04-14 15:07:19 +0530

    ADD: Gold DTO

--- backend/src/gold/dto/update-gold.dto.ts
+++ backend/src/gold/dto/update-gold.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateGoldDto } from './create-gold.dto';
 
-export class UpdateGoldDto extends PartialType(CreateGoldDto) {}
+export class UpdateGoldDto extends PartialType(
+  OmitType(CreateGoldDto, ['rateId'] as const),
+) {}
