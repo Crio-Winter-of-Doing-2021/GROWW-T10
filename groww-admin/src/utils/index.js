8893daa6538f86dcb090ab853ef37a40b2458738
Soham Pal <sohampal101@gmail.com>
2021-04-14 21:45:16 +0530

    UPD: Cleared unutilized files

--- groww-admin/src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Router } from './router';
-export { default as axios } from './axios';
-export { default as mock } from './mock';
--- groww-admin/src/utils/mock.js
+++
@@ -1,6 +0,0 @@
-import AxiosMockAdapter from 'axios-mock-adapter';
-import axios from './axios';
-
-const instance = new AxiosMockAdapter(axios, { delayResponse: 500 });
-
-export default instance;
